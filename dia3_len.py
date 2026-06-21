2ad95cf7fcc91b515f590e19bca5602d1ff49135
arthur-nery <arthur27nery@gmail.com>
2026-06-21 00:26:46 -0300

    Add Python day 3 exercise

---
+++ dia3_len.py
@@ -0,0 +1,15 @@
+cliente = input("Digite o nome do cliente: ")
+servico = input("Digite o serviço realizado: ")
+
+tamanho_cliente = len(cliente)
+tamanho_servico = len(servico)
+
+print("Resumo do registro:")
+print(cliente)
+print(servico)
+
+print("Caracteres no nome do cliente:")
+print(tamanho_cliente)
+
+print("Caracteres no serviço:")
+print(tamanho_servico)
