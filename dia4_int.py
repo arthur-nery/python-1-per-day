6dd85cd8830cf1e24061b646e9e545a89d8d3a7e
arthur-nery <arthur27nery@gmail.com>
2026-06-22 00:01:35 -0300

    Add Python day 4 exercise

---
+++ dia4_int.py
@@ -0,0 +1,10 @@
+cliente = input("Digite o nome do cliente: ")
+servico = input("Digite o serviço realizado: ")
+quantidade = input("Digite a quantidade de serviços: ")
+
+quantidade = int(quantidade)
+
+print("Registro criado:")
+print(cliente)
+print(servico)
+print(quantidade)
