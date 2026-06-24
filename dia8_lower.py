baa1cf3000cf1cce1bdeaaec6ed914952f5506a8
arthur-nery <arthur27nery@gmail.com>
2026-06-24 09:37:13 -0300

    Add Ruby day 8 exercise

---
+++ dia8_lower.py
@@ -0,0 +1,9 @@
+cliente = input("Digite o nome do cliente: ")
+servico = input("Digite o serviço realizado: ")
+
+cliente = cliente.lower()
+servico = servico.lower()
+
+print("Registro padronizado:")
+print(cliente)
+print(servico)
