d9cbc5f18e2154005c1a45a8e6db4ee6127d4ea0
arthur-nery <arthur27nery@gmail.com>
2026-06-25 09:19:03 -0300

    Add Python day 9 exercise

---
+++ dia9_upper.py
@@ -0,0 +1,10 @@
+cliente = input("Digite o nome do cliente: ")
+servico = input("Digite o serviço realizado: ")
+status = input("Digite o status do pagamento: ")
+
+status = status.upper()
+
+print("Registro criado:")
+print(cliente)
+print(servico)
+print(status)
