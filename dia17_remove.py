1275dc43f10c57f3e84fdf8a5d8f35ccdc026210
arthur-nery <arthur27nery@gmail.com>
2026-07-06 09:27:06 -0300

    Add Python day 17 exercise

---
+++ dia17_remove.py
@@ -0,0 +1,24 @@
+cliente = input("Digite o nome do cliente: ")
+
+servicos = []
+
+servico_1 = input("Digite o primeiro serviço: ")
+servico_2 = input("Digite o segundo serviço: ")
+servico_3 = input("Digite o terceiro serviço: ")
+
+servicos.append(servico_1)
+servicos.append(servico_2)
+servicos.append(servico_3)
+
+print("Serviços registrados:")
+print(servicos)
+
+remover = input("Digite exatamente o serviço que deseja remover: ")
+
+servicos.remove(remover)
+
+print("Cliente:")
+print(cliente)
+
+print("Serviços finais:")
+print(servicos)
