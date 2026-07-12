3bddf690a80ea031d2bbecfa70c1d9775a7ff796
arthur-nery <arthur27nery@gmail.com>
2026-07-12 19:48:14 -0300

    Add Python day 23 exercise

---
+++ dia23_insert.py
@@ -0,0 +1,22 @@
+cliente = input("Digite o nome do cliente: ")
+
+servicos = []
+
+servico_1 = input("Digite o primeiro serviço: ")
+servico_2 = input("Digite o segundo serviço: ")
+
+servicos.append(servico_1)
+servicos.append(servico_2)
+
+print("Serviços antes da correção:")
+print(servicos)
+
+servico_esquecido = input("Digite o serviço esquecido: ")
+
+servicos.insert(1, servico_esquecido)
+
+print("Cliente:")
+print(cliente)
+
+print("Serviços depois da correção:")
+print(servicos)
